ed9e4e2a4e566f5fc8b9deae8fd5702a96d2e59e
Satyam <bajpai1973satyam@gmail.com>
2024-03-24 23:46:33 +0530

    feat: add copy clipboard on color card component

--- src/components/ColorCard/ColorCard.tsx
+++ src/components/ColorCard/ColorCard.tsx
@@ -1,13 +1,57 @@
+import { useEffect, useState } from 'react';
 import { ColorCardProps } from './colorCard.types';
 import styles from './colorCard.module.css';
 
+const COPY_CLIPBOARD_ICON_TIMEOUT_TIME = 1000;
+
 function ColorCard({ colorName, colorCode }: ColorCardProps) {
+  const [isCardHovered, setIsCardHovered] = useState<boolean>(false);
+  const [isCopiedSuccessfully, setIsCopiedSuccessfully] =
+    useState<boolean>(false);
+
+  let timeout: NodeJS.Timeout;
+
+  useEffect(() => {
+    return () => clearTimeout(timeout);
+  }, []);
+
+  function copyColorHandler() {
+    window.navigator.clipboard.writeText(colorCode).then(() => {
+      setIsCopiedSuccessfully(true);
+      timeout = setTimeout(() => {
+        setIsCopiedSuccessfully(false);
+      }, COPY_CLIPBOARD_ICON_TIMEOUT_TIME);
+    });
+  }
+
+  function onHoverOnCard() {
+    setIsCardHovered(true);
+  }
+
+  function onMouseOutOnCard() {
+    setIsCardHovered(false);
+  }
+  
   return (
     <div
       className={styles['color-card']}
       style={{ backgroundColor: colorCode }}
+      onMouseOver={onHoverOnCard}
+      onMouseOut={onMouseOutOnCard}
     >
       {colorName}
+      {isCardHovered && (
+        <button
+          onClick={copyColorHandler}
+          className={styles['color-card__copy-button']}
+        >
+          {isCopiedSuccessfully ? (
+            <img src='/copy-tick-icon.svg' alt='copy successful icon' />
+          ) : (
+            <img src='/copy-icon.svg' alt='copy icon' />
+          )}
+        </button>
+      )}
     </div>
   );
 }
