de5da32dc18c8103d9d18516710d9de115684e75
Satyam <bajpai1973satyam@gmail.com>
2024-03-24 23:51:57 +0530

    feat: add favicon and title

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,5 +2,6 @@ import styles from './footer.module.css';
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
+  
   return <footer className={styles.footer}>&copy; Copyright {currentYear}. All rights reserved.</footer>;
 }
